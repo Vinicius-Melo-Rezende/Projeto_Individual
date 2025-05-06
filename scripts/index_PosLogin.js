a1a01f13c6a9e78566a71d03ff4b321e797e4d82
Vinicius-Melo-Rezende <vinicius.rezende@sptech.school>
2025-05-06 11:20:58 -0300

    subindo os arquivos

--- scripts/index_PosLogin.js
+++ scripts/index_PosLogin.js
@@ -12,38 +12,3 @@ function SaiuTopo(){
 
 addEventListener('scroll', SaiuTopo)
 
-var Missao = document.getElementById('Missao');
-var aMissao = document.getElementById('aMissao');
-
-aMissao.addEventListener('click', function (){
-    Missao.scrollIntoView({ block: "center", behavior: "smooth" });;
-})
-
-var Inicio = document.getElementById('apresentacao');
-var aInicio = document.getElementById('aInicio');
-
-aInicio.addEventListener('click', function (){
-    Inicio.scrollIntoView({ block: "center", behavior: "smooth" });;
-})
-
-var QmSomos = document.getElementById('QmSomos');
-var aQmSomos = document.getElementById('aQmSomos');
-
-
-aQmSomos.addEventListener('click', function (){
-    QmSomos.scrollIntoView({ block: "center", behavior: "smooth" });;
-})
-aSBMais.addEventListener('click', function (){
-    QmSomos.scrollIntoView({ block: "center", behavior: "smooth" });;
-})
-aSBMais.addEventListener('click', function (){
-    QmSomos.scrollIntoView({ block: "center", behavior: "smooth" });;
-})
-
-var Contato = document.getElementById('Contato');
-var aContato = document.getElementById('aContato');
-
-aContato.addEventListener('click', function (){
-    Contato.scrollIntoView({ block: "center", behavior: "smooth" });;
-})
-
