7d1530abceabffabb6f3524e0171fd3038d48ece
Vinicius-Melo-Rezende <vinicius.rezende@sptech.school>
2025-05-07 15:35:26 -0300

    Atualizando os arquivos dos sites

--- scripts/login_cadastro.js
+++ scripts/login_cadastro.js
@@ -6,13 +6,13 @@ const formCadastro = document.getElementById('formCadastro');
 
 
 btnControlCadastro.addEventListener('click', function(){
-    btnControlCadastro.style.backgroundColor = '#4E2B06';
+    btnControlCadastro.style.backgroundColor = '#ff0000';
     btnControlLogin.style.backgroundColor = '#2E2F2F';
     formLogin.classList.add('d-none');
     formCadastro.classList.remove('d-none');
 })
 btnControlLogin.addEventListener('click', function(){
-    btnControlLogin.style.backgroundColor = '#4E2B06';
+    btnControlLogin.style.backgroundColor = '#ff0000';
     btnControlCadastro.style.backgroundColor = '#2E2F2F';
     formLogin.classList.remove('d-none');
     formCadastro.classList.add('d-none')
